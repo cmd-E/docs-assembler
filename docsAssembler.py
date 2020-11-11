6058ee82da8f335f846e58168fa69be6c4c4e390
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 16:38:55 +0600

    added main functions and options

--- docsAssembler.py
+++ docsAssembler.py
@@ -1,34 +1,69 @@
-import docx
-import os
+import docx, os, sys, getopt
+
+filename = "AssemledDocs.docx"
 
 def getAllDocx(directory = os.getcwd()):
     files = []
-    for f in os.listdir(directory):
-        if f.endswith('.docx') or f.endswith('.doc'):
-            files.append(f)
+    try:
+        for f in os.listdir(directory):
+            if f.endswith('.docx') or f.endswith('.doc'):
+                files.append(f)
+    except:
+        print("В этой директории нет документов")
+        sys.exit()
     return files
 
 # start recording text on "ЭКЗАМЕНАЦИОННЫЙ БИЛЕТ № **" end on "Составитель ст.преподаватель кафедры «РЭТ»                           Наурыз К.Ж."
-def copyAllDocsToOne(path, mainDoc, docs):
+def copyAllDocsToOne(path, mainDoc, docs, startPhrase, endPhrase):
     for docName in docs:
         copyFlag = False
         paragraphsToCopy = []
-        partOfEverythingDoc = docx.Document(os.path.join(path, docName))
+        try:
+            partOfEverythingDoc = docx.Document(os.path.join(path, docName))
+        except:
+            print("В этой директории нет документов")
+            sys.exit()
         for paragraph in partOfEverythingDoc.paragraphs:
-            if "ЭКЗАМЕНАЦИОННЫЙ БИЛЕТ" in paragraph.text:
+            if startPhrase in paragraph.text:
                 copyFlag = True
-            elif "Составитель ст.преподаватель кафедры «РЭТ»                           Наурыз К.Ж." in paragraph.text:
+            elif endPhrase in paragraph.text:
                 copyFlag = False
             if copyFlag:
                 paragraphsToCopy.append(paragraph.text)
         addParagraphsToMainDoc(mainDoc, paragraphsToCopy)
-    mainDoc.save('AssemledDocs.docx')
+        print(f"{docName} скопирован")
+    if len(docs) != 0:
+        mainDoc.save(filename)
+        print(f"Результат: {os.path.join(os.getcwd(), filename)}")
 
 def addParagraphsToMainDoc(mainDoc, paragraphsToCopy):
     for i in paragraphsToCopy:
         mainDoc.add_paragraph(i)
-path = '/home/deus'
-docs = getAllDocx(path)
-mainDoc = docx.Document()
-copyAllDocsToOne(path, mainDoc, docs)
 
+def main(argv):
+    path = '/home/deus/'
+    startPhrase = ''
+    endPhrase = ''
+    try:
+        opts, _ = getopt.getopt(argv,"hp:s:e:",["path=","startph=","endph="])
+    except getopt.GetoptError:
+        print('docsAssembler.py p <path> s <startphrase> -e <endphrase>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('docsAssembler.py p <path> s <startphrase> -e <endphrase>')
+            sys.exit()
+        elif opt in ("-s", "--startph"):
+            startPhrase = arg
+        elif opt in ("-e", "--endph"):
+            endPhrase = arg
+        elif opt in ("-p", "--path"):
+            path = arg
+    if path == "":
+        path = os.getcwd()
+    docs = getAllDocx(path)
+    mainDoc = docx.Document()
+    copyAllDocsToOne(path, mainDoc, docs, startPhrase, endPhrase)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
