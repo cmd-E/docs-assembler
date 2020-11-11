24e8d8399e08553d8d2139551c4b2cc6fa5facc7
IrvinIrvin <perevyshink_1@mail.ru>
2020-11-11 16:08:26 +0600

    added git ignore, added method to copy necessary content to one file

--- docsAssembler.py
+++ docsAssembler.py
@@ -4,7 +4,31 @@ import os
 def getAllDocx(directory = os.getcwd()):
     files = []
     for f in os.listdir(directory):
-        if f.endswith('.docx') or f.endswith('.doc') or f.endswith('.odt'):
+        if f.endswith('.docx') or f.endswith('.doc'):
             files.append(f)
     return files
-print(getAllDocx('/home/deus'))
+
+# start recording text on "ЭКЗАМЕНАЦИОННЫЙ БИЛЕТ № **" end on "Составитель ст.преподаватель кафедры «РЭТ»                           Наурыз К.Ж."
+def copyAllDocsToOne(path, mainDoc, docs):
+    for docName in docs:
+        copyFlag = False
+        paragraphsToCopy = []
+        partOfEverythingDoc = docx.Document(os.path.join(path, docName))
+        for paragraph in partOfEverythingDoc.paragraphs:
+            if "ЭКЗАМЕНАЦИОННЫЙ БИЛЕТ" in paragraph.text:
+                copyFlag = True
+            elif "Составитель ст.преподаватель кафедры «РЭТ»                           Наурыз К.Ж." in paragraph.text:
+                copyFlag = False
+            if copyFlag:
+                paragraphsToCopy.append(paragraph.text)
+        addParagraphsToMainDoc(mainDoc, paragraphsToCopy)
+    mainDoc.save('AssemledDocs.docx')
+
+def addParagraphsToMainDoc(mainDoc, paragraphsToCopy):
+    for i in paragraphsToCopy:
+        mainDoc.add_paragraph(i)
+path = '/home/deus'
+docs = getAllDocx(path)
+mainDoc = docx.Document()
+copyAllDocsToOne(path, mainDoc, docs)
+
